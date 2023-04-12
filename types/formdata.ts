6759adf42fbb0d322c885689d36f0f07f6c8e803
Matt Brooks <matt@techequitycollaborative.org>
2023-04-12 15:59:16 -0700

    added type definitions for form data

---
+++ types/formdata.ts
@@ -0,0 +1,37 @@
+import { Race, EvictionReason, CriminalHistoryType, ApplicationMethod, AssessmentOutcome, DenialReason } from './formoptions';
+
+export interface FormData {
+  email: string,
+  race: string,
+  age: number | undefined,
+  yearlyIncome: number | undefined,
+  creditScore: number | undefined,
+  evictionHistory: Array<Eviction>,
+  criminalHistory: Array<CriminalHistoryEntry>,
+  street: string,
+  unit: string,
+  city: string,
+  zipcode: string,
+  monthlyRent: number | undefined,
+  landlordName: string,
+  screeningCompanyName: string,
+  screeningFee: number | undefined,
+  portableScreeningFee: "Yes" | "No" | undefined,
+  applicationMethod: ApplicationMethod | undefined,
+  assessmentOutcome: AssessmentOutcome | undefined,
+  assessmentOutcomeDetails: string,
+  denialReason: DenialReason | undefined,
+  otherDenialReason: string,
+  additionalContextNotes: string,
+};
+
+export interface Eviction {
+  evictionReason: EvictionReason,
+  evictionDate: Date,
+};
+
+export interface CriminalHistoryEntry {
+  criminalHistoryType: CriminalHistoryType,
+  convictionDate: Date,
+  offenseName: string,
+}
---
+++ types/formoptions.ts
@@ -0,0 +1,38 @@
+export enum Race {
+  "American Indian or Alaska Native",
+  "Asian",
+  "Black or African American",
+  "Native Hawaiian or Other Pacific Islander",
+  "White",
+  "Two or More Races",
+  "No Response",
+};
+
+export enum EvictionReason {
+  "Reason 1",
+  "Reason 2",
+  "Reason 3",
+};
+
+export enum CriminalHistoryType {
+  "Reason 1",
+  "Reason 2",
+  "Reason 3",
+};
+
+export enum ApplicationMethod {
+  "Method 1",
+  "Method 2",
+  "Method 3",
+};
+
+export enum AssessmentOutcome {
+  "Accepted",
+  "Denied",
+};
+
+export enum DenialReason {
+  "Reason 1",
+  "Reason 2",
+  "Reason 3",
+}
