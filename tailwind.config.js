c72dd213d75914a46d69e07451192142537b86be
Conrad Shock <shockconrad@gmail.com>
2023-04-20 14:49:47 -0700

    starting color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      /* partial color scheme while waiting for finalization */
+      transparent: "transparent",
+      current: "currentColor",
+      navy: {
+        DEFAULT: "#172f6e",
+        deep: "#112353",
+        light: "#45598b",
+        lighter: "#7482a8",
+      },
+      blue: "#006ac3",
+      "blue-gray": "#9fa9b4",
+      mist: "#e9f3f8",
+      "dark-blue-gray": "#696c78",
+    },
     extend: {},
   },
   plugins: [],
