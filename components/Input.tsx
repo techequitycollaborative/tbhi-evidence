ea5fe5074fab9c672d565be21eb74c9c3b416aa1
Conrad Shock <shockconrad@gmail.com>
2023-04-27 01:17:21 -0700

    fix 'uncontrolled to controlled' input warning

--- components/Input.tsx
+++ components/Input.tsx
@@ -78,7 +78,7 @@ export default function Input(props: FormInputProps) {
           id={labelId}
           placeholder={placeholder}
           type={type}
-          value={value}
+          value={value || ""}
           onChange={onChange}
           onFocus={() => setType(props.type)}
         />
